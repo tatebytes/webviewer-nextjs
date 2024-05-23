dd3b0cb9bbcd0170a0efcd43e4806dd15ad70486
Tate <tatebytes@gmail.com>
2024-05-23 15:50:59 +0800

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function HomePage() {
   const viewer = useRef(null);
+  const [instance, setInstance] = useState(null);
 
   useEffect(() => {
-    import('@pdftron/webviewer').then(() => {
-      WebViewer(
-        {
-          path: '/webviewer/lib',
-          initialDoc: '/files/PDFTRON_about.pdf',
-          licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
-        },
-        viewer.current
-      ).then((instance) => {
-        const { docViewer } = instance;
-        // you can now call WebViewer APIs here...
+    if (!instance) {
+      import('@pdftron/webviewer').then(() => {
+        WebViewer(
+          {
+            path: '/webviewer/lib',
+            initialDoc: '/files/PDFTRON_about.pdf',
+            licenseKey: 'your_license_key',  // sign up to get a free trial key at https://dev.apryse.com
+          },
+          viewer.current
+        ).then((wvInstance) => {
+          setInstance(wvInstance);
+          const { docViewer } = wvInstance;
+          // you can now call WebViewer APIs here...
+        });
       });
-    });
-  }, []);
+    }
+  }, [instance]);
 
   return (
     <div className='MyComponent'>
